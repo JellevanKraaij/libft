403a4cb37720a179fda6479b61e0e95410d510f2
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-02-04 22:17:33 +0100

    fixed norm error

--- ft_reallocf.c
+++ ft_reallocf.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_reallocf.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jvan-kra <jvan-kra@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/02/04 21:36:43 by jvan-kra      #+#    #+#                 */
+/*   Updated: 2022/02/04 21:36:43 by jvan-kra      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void *ft_reallocf(void *old, size_t oldsize, size_t newsize)
+void	*ft_reallocf(void *old, size_t oldsize, size_t newsize)
 {
-	void *ret;
-	
+	void	*ret;
+
 	ret = malloc(newsize);
-	if(ret == NULL)
+	if (ret == NULL)
 	{
 		free(old);
 		return (NULL);
